f87a66dcb033af344c123bf4834ef0eec1fb400a
replication <replication@gmail.com>
2011-05-31 22:48:42 +0900

    エラー処理を追加しました。

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /**
  * Module dependencies.
  */
-
+require.paths.push('/usr/local/lib/node_modules');
 var express = require('express');
 var sys = require('sys');
 
@@ -29,77 +29,91 @@ app.configure('production', function(){
 var pg = require('pg');
 var connectionString = "tcp://ユーザID:パスワード@localhost:5432/データベース名";
 
+var title = "node.js + PostgreSQL";
 var action;
-var text;
-var selected;
+var errorMsg = "";
 
 // Routes
 app.get('/', function(req, res){
-  pg.connect(connectionString, function(err, client) {
-    var query = client.query('SELECT * FROM test');
-    var rows = [];
-    query.on('row', function(row) {
-      rows.push(row);
-    });
-    query.on('end', function(row) {
-      res.render('index', {
-        title: 'node.js + PostgreSQL',
-        data: rows
-      });
-    });
-  });
+	pg.connect(connectionString, function(error, client) {
+		var rows = [];
+		var query = client.query('SELECT * FROM test');
+		query.on('error', function(error) {
+			console.log("error event stat...");
+			errorMsg = sys.inspect(error);
+			console.log(errorMsg);
+			res.render('index', {
+				title: title,
+				data: rows,
+				message: errorMsg
+			});
+		});
+		query.on('row', function(row) {
+			console.log("row event start...");
+			rows.push(row);
+		});
+		query.on('end', function(row, error) {
+			console.log("end event start...");
+			if(!errorMsg) {
+				res.render('index', {
+					title: title,
+					data: rows,
+					message: errorMsg
+				});
+			}
+		});
+	});
 });
 
 app.post('/', function(req, res){
-  action = req.body.action;
-  text = req.body.text;
-  selected = req.body.selected;
+	action = req.body.action;
 
-  // callback function
-  var callback = function(err, client) {
-    if(err) {
-      res.writeHead(500, {"Content-Type" : "text/plain"});
-      return res.end("Error! " + sys.inspect(err))
-    }
-    // 検索のときはクエリを変える
-    if(action === "search") {
-      var query = client.query('SELECT * FROM test WHERE NAME = $1', [text]);
-    }
-    else {
-      var query = client.query('SELECT * FROM test');
-    }
-    var rows = [];
-    query.on('row', function(row) {
-      rows.push(row);
-    });
-    query.on('end', function(row) {
-      res.render('index', {
-        title: 'node.js + PostgreSQL',
-        data: rows
-      });
-    });
-  }
-  
-  pg.connect(connectionString, function(err, client) {
-    // 新規登録
-    var query;
-    if(action === "create") {
-      query = client.query('INSERT INTO test(name) VALUES($1)', [text], callback(err, client));
-    }
-    // 更新
-    else if(action === "update") {
-      query = client.query('UPDATE test SET NAME = $1 WHERE ID = $2', [text, selected], callback(err, client));
-    }
-    // 削除
-    else if(action === "delete") {
-      query = client.query('DELETE FROM test WHERE ID = $1', [selected], callback(err, client));
-    }
-    // 上記以外
-    else {
-      callback(err, client);
-      return true;
-    }
-  });
+	var show = function(error, client) {
+		// 検索
+		var query;
+		if(action === "search") {
+			query = client.query('SELECT * FROM test WHERE NAME = $1', [req.body.text]);
+		}
+		// 上記以外
+		else {
+			query = client.query('SELECT * FROM test');
+		}
+		var rows = [];
+		query.on('error', function(error) {
+			console.log("error event stat...");
+			errorMsg = sys.inspect(error);
+			console.log(errorMsg);
+		});
+		query.on('row', function(row) {
+			rows.push(row);
+		});
+		query.on('end', function(row) {
+			res.render('index', {
+				title: 'node.js + PostgreSQL',
+				data: rows,
+				message: errorMsg
+			});
+		});
+	}
+	
+	pg.connect(connectionString, function(error, client) {
+		var query;
+		switch(action) {
+			// 新規登録
+			case "create":
+				query = client.query('INSERT INTO test(name) VALUES($1)', [req.body.text]);
+				break;
+			// 更新
+			case "update":
+				query = client.query('UPDATE test SET NAME = $1 WHERE ID = $2', [req.body.text, req.body.selected]);
+				break;
+			// 削除
+			case "delete":
+				query = client.query('DELETE FROM test WHERE ID = $1', [req.body.selected]);
+				break;
+		}
+		show(error, client);
+	});
 });
 
 // Only listen on $ node app.js
